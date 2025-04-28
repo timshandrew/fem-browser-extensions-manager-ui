fed34b44c6578b24a9c9a65ef0d1b97c84010de0
Andrew <andrew.alan.sm@gmail.com>
2025-04-28 15:12:02 +0100

    style extension filter options

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
 import "./App.css";
 import Card from "./components/Card";
+import logo from "./assets/images/logo.svg";
+import sunIcon from "./assets/images/icon-sun.svg";
+import moonIcon from "./assets/images/icon-moon.svg";
 
 function App() {
     return (
         <>
-            <Card
-                logoSrc="./images/logo-style-spy.svg"
-                name="Card name"
-                description="This is a card description."
-                isActive={true}
-            />
+            <header>
+                <img src={logo} alt="Project logo" />
+                <button type="button" className="theme-toggle">
+                    <img src={sunIcon} alt="Sun icon" className="sun-icon" />
+                </button>
+            </header>
+
+            <main>
+                <div className="extension-select-header">
+                    <h2>Extensions List</h2>
+                    <div className="filter-options">
+                        <label>
+                            All
+                            <input type="radio" name="filter" value="all" />
+                        </label>
+
+                        <label>
+                            Active
+                            <input type="radio" name="filter" value="active" />
+                        </label>
+
+                        <label>
+                            Inactive
+                            <input
+                                type="radio"
+                                name="filter"
+                                value="inactive"
+                            />
+                        </label>
+                    </div>
+                </div>
+                <Card
+                    logoSrc="./images/logo-style-spy.svg"
+                    name="Card name"
+                    description="This is a card description."
+                    isActive={true}
+                />
+            </main>
         </>
     );
 }
